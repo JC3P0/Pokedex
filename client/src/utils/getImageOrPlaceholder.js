6506ae80c19dad5bb1b1af19f058f27018052a09
Josh Clemens <josh.clemens@outlook.com>
2024-08-30 19:59:17 -0700

    update hover type view

--- client/src/utils/getImageOrPlaceholder.js
+++ client/src/utils/getImageOrPlaceholder.js
@@ -1,6 +1,6 @@
 // src/utils/getImageOrPlaceholder.js
 
-const placeholderImageUrl = 'https://raw.githubusercontent.com/JC3P0/Pokedex/main/client/src/assets/noImg.png';
+const placeholderImageUrl = 'https://raw.githubusercontent.com/JC3P0/Pokedex/main/client/src/assets/noImg-2.png';
 
 export const getImageOrPlaceholder = (spriteUrl) => {
   return spriteUrl ? spriteUrl : placeholderImageUrl;
