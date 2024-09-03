b5ff9c4726d084b7ef46bc626745a795326211e2
Josh Clemens <josh.clemens@outlook.com>
2024-09-03 13:27:05 -0700

    remove unused functions

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -31,49 +31,6 @@ export const fetchPokemon = async () => {
     return await fetchPaginatedData('/getPokemon');
 };
 
-// Function to fetch a Pokémon by its ID
-export const fetchPokemonById = async (id) => {
-    try {
-        const response = await api.get(`/getPokemon?query=${id}`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching Pokémon by ID:', error);
-        throw error;
-    }
-};
-
 export const fetchItems = async () => {
     return await fetchPaginatedData('/getItem');
-};
-
-// Function to fetch an item by its ID
-export const fetchItemById = async (id) => {
-    try {
-        const response = await api.get(`/getItem?query=${id}`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching Item by ID:', error);
-        throw error;
-    }
-};
-
-// Function to search Pokémon by a search term
-export const searchPokemon = async (searchTerm) => {
-    try {
-        const response = await api.get(`/getPokemon?query=${searchTerm}`);
-        return response.data;
-    } catch (error) {
-        console.error('Error searching Pokémon:', error);
-        throw error;
-    }
-};
-
-// Function to toggle the favorite status of an entity (Pokémon or item)
-export const toggleFavorite = async (id, entity) => {
-    try {
-        // Placeholder for any API request to save/remove the favorite on a server if needed
-    } catch (error) {
-        console.error('Error toggling favorite:', error);
-        throw error;
-    }
 };
